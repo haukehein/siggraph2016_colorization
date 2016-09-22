9ed1e854f1e1b3e32325da683bbb8ba367bddb48
haukehein <BitCrusher@gmx-topmail.de>
2016-09-22 16:12:25 +0200

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-docker build $1 -t juron/siggraph2016_colorization:2016-04-25 .
 
+[ -z "$( which git        2> /dev/null )" ]  &&  echo "ERROR: 'git' is not installed!"                   &&  exit 1
+[ -z "$( which docker     2> /dev/null )" ]  &&  echo "ERROR: 'docker' is not installed!"                &&  exit 1
+[ -z "$( docker --version 2> /dev/null )" ]  &&  echo "ERROR: You are not allowed to execute 'docker'!"  &&  exit 1
+
+if [ -z "$( docker images haukehein/torch7  2> /dev/null | grep 1.0.0 )" ]
+then
+	TMP=`mktemp -d`
+		git clone https://github.com/haukehein/torch7.git "$TMP" --recursive  && \
+		cd "$TMP"  && \
+		bash ./build.sh
+	cd -  
+	rm -rf "$TMP"
+fi
+
+docker build $1 -t juron/siggraph2016_colorization:2016-04-25 .
