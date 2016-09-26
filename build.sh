9d217918357758d7eb88b2ea366257b8a519a28a
haukehein <BitCrusher@gmx-topmail.de>
2016-09-26 09:26:26 +0200

    Add files via upload

--- build.sh
+++ build.sh
@@ -5,14 +5,18 @@
 [ -z "$( which docker     2> /dev/null )" ]  &&  echo "ERROR: 'docker' is not installed!"                &&  exit 1
 [ -z "$( docker --version 2> /dev/null )" ]  &&  echo "ERROR: You are not allowed to execute 'docker'!"  &&  exit 1
 
-if [ -z "$( docker images haukehein/torch7  2> /dev/null | grep 1.0.0 )" ]
+
+  BUILD=haukehein/siggraph2016_colorization:2016-04-25
+  FROM=haukehein/torch7:1.0.0
+
+if [ -z "$( docker images $(echo -E "$FROM" | cut -d: -f1)  2> /dev/null | grep $(echo -E "$FROM" | cut -d: -f2) )" ]
 then
-	TMP=`mktemp -d`
-		git clone https://github.com/haukehein/torch7.git "$TMP" --recursive  && \
-		cd "$TMP"  && \
-		bash ./build.sh
-	cd -  
-	rm -rf "$TMP"
+    TMP=`mktemp -d`
+	git clone https://github.com/$(echo -E "$FROM" | cut -d: -f1).git "$TMP" --recursive  && \
+	cd "$TMP"  && \
+	bash ./build.sh
+    cd -  
+    rm -rf "$TMP"
 fi
 
-docker build $1 -t haukehein/siggraph2016_colorization:2016-04-25 .
+docker build $1 -t $BUILD .
